2512c7b6cdf296752b99c1ec5ca9b2d928d02ce8
Alina Walling <alina.walling@duke.edu>
2018-09-13 19:12:56 -0400

    Started implementing interface

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,88 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
+<div id="app">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-4">
+                <div id="languages" class="card-shadow d-flex flex-row justify-content-between">
+                    <div class="dropdown">
+                        <button class="btn dropdown-toggle language-button" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                          Language 1
+                          </button>
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                            <a class="dropdown-item" href="#">Action</a>
+                            <a class="dropdown-item" href="#">Another action</a>
+                            <a class="dropdown-item" href="#">Something else here</a>
+                        </div>
+                    </div>
+                    <div class="dropdown">
+                        <button class="btn dropdown-toggle language-button" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                          Language 2
+                          </button>
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                            <a class="dropdown-item" href="#">Action</a>
+                            <a class="dropdown-item" href="#">Another action</a>
+                            <a class="dropdown-item" href="#">Something else here</a>
+                        </div>
+                    </div>
+                </div>
+                <div id="chat-window" class="card-shadow">
+                  <div class="d-flex flex-row">
+                  <input class="form-control" type="text" placeholder="Default input">
+                  <button class="btn btn-primary"><font-awesome-icon icon="microphone" /></button>
+                  </div>
+                </div>
+            </div>
+            <div class="col">
+                yeet
+            </div>
+        </div>
+    </div>
+</div>
 </template>
 
 <script>
 export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+    name: 'app',
+    data() {
+        return {
+            msg: 'Welcome to Your Vue.js App'
+        }
     }
-  }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    background-color: #F7F7FA;
 }
 
-h1, h2 {
-  font-weight: normal;
+.card-shadow {
+    border-radius: 12px;
+    -webkit-box-shadow: 0px 4px 16px 0px rgba(69, 91, 99, 0.08);
+    -moz-box-shadow: 0px 4px 16px 0px rgba(69, 91, 99, 0.08);
+    box-shadow: 0px 4px 16px 0px rgba(69, 91, 99, 0.08);
 }
 
-ul {
-  list-style-type: none;
-  padding: 0;
+#languages {
+    background-color: #2A2E43;
+    margin-bottom: 12px;
+    padding: 8px 16px;
 }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
+.language-button {
+  background-color: transparent;
+  color: white;
 }
 
-a {
-  color: #42b983;
+#chat-window {
+    height: 300px;
+    background-color: white;
+    padding: 16px;
+}
+
+.divider-v {
+  width: 1px;
+  background-color: #454F63;
+  height: 40px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
+import "bootstrap/dist/css/bootstrap.css"
+
 import Vue from 'vue'
 import App from './App.vue'
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faCoffee)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 new Vue({
   el: '#app',
